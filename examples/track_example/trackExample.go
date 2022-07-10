a3cfa372990066951a37cff7104170b221f12c2c
Xinyi Ye <xinyi.ye@amplitude.com>
2022-07-10 14:45:20 -0700

    feat: new example

--- examples/track_example/trackExample.go
+++ examples/track_example/trackExample.go
@@ -1,13 +1,34 @@
-package track_example
+// A basic example of using Amplitude Go SDK to track an event.
 
-import "github.com/amplitude/Amplitude-Go/pkg/amplitude"
+package main
 
+// Import amplitude package
+import (
+	"github.com/amplitude/Amplitude-Go/amplitude"
+)
+
+// Define your callback function (optional)
 func callbackFunc(e string, code int, message string) {
 	println(e)
 	println(code, message)
 }
 
-var config amplitude.Config = amplitude.Config{ApiKey: "your_api_key"}
-var client amplitude.Amplitude = amplitude.Amplitude{Configuration: config}
+func main() {
 
-var event amplitude.BaseEvent = amplitude.BaseEvent{}
+	var config = amplitude.Config{ApiKey: "your_api_key"}
+
+	// Config callback function (optional)
+	var client = amplitude.Amplitude{Configuration: config}
+
+	// Create a BaseEvent instance
+	var event = amplitude.BaseEvent{}
+
+	// Track an event
+	client.Track(event)
+
+	// Flush the event buffer
+	client.Flush()
+
+	// Shutdown the client
+	client.Shutdown()
+}
