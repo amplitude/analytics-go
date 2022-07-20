2419f720a36ef63624f2344e5c6728c1410eafd4
Andrey Sokolov <andrey.sokolov@amplitude.com>
2022-07-20 19:58:09 +0400

    temp: plugins

--- amplitude/context_plugin.go
+++ amplitude/context_plugin.go
@@ -1,7 +1,38 @@
 package amplitude
 
+import "time"
+
 // ContextPlugin is the default plugin that add library info to event.
 // It also sets event default timestamp and insert_id if not set elsewhere
 type ContextPlugin struct {
-	BasePlugin
+	contextString string
+}
+
+func NewContextPlugin() *ContextPlugin {
+	return &ContextPlugin{
+		contextString: SdkLibrary + "/" + SdkVersion,
+	}
+}
+
+func (c *ContextPlugin) Setup(config Config) {
+}
+
+func (c *ContextPlugin) Priority() MiddlewarePriority {
+	return MiddlewarePriorityBefore
+}
+
+// Execute sets default timestamp and insertID if not set elsewhere
+// It also adds SDK name and version to event library.
+func (c *ContextPlugin) Execute(event *Event) *Event {
+	if event.time.IsZero() {
+		event.time = time.Now()
+	}
+
+	if event.insertID == "" {
+		event.insertID = uuid.NewString()
+	}
+
+	event.library = c.contextString
+
+	return event
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/amplitude/Amplitude-Go
 
 go 1.17
+
+require github.com/google/uuid v1.3.0
