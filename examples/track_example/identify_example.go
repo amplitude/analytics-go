3e32a732285e3c8378c5c62177794fd69614b20a
Xinyi Ye <xinyi.ye@amplitude.com>
2022-08-03 21:43:25 -0700

    chore: add identify example

---
+++ examples/track_example/identify_example.go
@@ -0,0 +1,40 @@
+// A basic example of using Amplitude Go SDK to set user property
+
+package main
+
+// Import amplitude package
+import (
+	"github.com/amplitude/Amplitude-Go/amplitude"
+)
+
+func main() {
+
+	config := amplitude.NewConfig("your_api_key")
+
+	// Config callback function (optional)
+	client := amplitude.NewClient(config)
+
+	// Identify struct provides controls over setting user properties.
+	identifyObj := amplitude.Identify{}
+
+	// Set the value of a user property
+	identifyObj.Set("location", "LAX")
+
+	// Call Identify method of client
+	client.Identify(identifyObj, amplitude.EventOptions{UserID: "identify-user-id"})
+
+	// Create a BaseEvent instance
+	event := amplitude.Event{
+		EventOptions: amplitude.EventOptions{DeviceID: "go-device-id", UserID: "identify-user-id"},
+		EventType:    "identify-event-type",
+	}
+
+	// Track an event
+	client.Track(event)
+
+	// Flush the event buffer
+	client.Flush()
+
+	// Shutdown the client
+	client.Shutdown()
+}
