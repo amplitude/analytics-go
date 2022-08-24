a6d35e3be25aeea86a68bb52a6686c816e9f1725
Xinyi Ye <xinyi.ye@amplitude.com>
2022-08-23 21:18:01 -0700

    revert logger

--- amplitude/logger.go
+++ amplitude/logger.go
@@ -6,34 +6,34 @@ import (
 )
 
 type Logger interface {
-	Debug(args ...interface{})
-	Info(args ...interface{})
-	Warn(args ...interface{})
-	Error(args ...interface{})
+	Debug(message string, args ...interface{})
+	Info(message string, args ...interface{})
+	Warn(message string, args ...interface{})
+	Error(message string, args ...interface{})
 }
 
 type stdLogger struct {
 	logger *log.Logger
 }
 
-func (l *stdLogger) Debug(args ...interface{}) {
-	l.logger.Printf("Debug: ", args...)
+func (l *stdLogger) Debug(message string, args ...interface{}) {
+	l.logger.Printf("Debug: "+message, args...)
 }
 
-func (l *stdLogger) Info(args ...interface{}) {
-	l.logger.Printf("Info: ", args...)
+func (l *stdLogger) Info(message string, args ...interface{}) {
+	l.logger.Printf("Info: "+message, args...)
 }
 
-func (l *stdLogger) Warn(args ...interface{}) {
-	l.logger.Printf("Warn: ", args...)
+func (l *stdLogger) Warn(message string, args ...interface{}) {
+	l.logger.Printf("Warn: "+message, args...)
 }
 
-func (l *stdLogger) Error(args ...interface{}) {
-	l.logger.Printf("Error: ", args...)
+func (l *stdLogger) Error(message string, args ...interface{}) {
+	l.logger.Printf("Error: "+message, args...)
 }
 
-func NewDefaultLogger() Logger {
+func newDefaultLogger() Logger {
 	return &stdLogger{logger: log.New(os.Stderr, "amplitude-analytics", log.LstdFlags)}
 }
 
-var globalLogger = NewDefaultLogger()
+var globalLogger = newDefaultLogger()
